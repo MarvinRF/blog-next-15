a98cdf638c423fc8a0c1c539c0325198848713a3
Marvin Rocha <marvinvca@outlook.com>
2025-06-19 17:43:26 -0300

    Update deps, layout & simulation delay
    Adds Tailwind stylesheet config and updates dependency versions (autoprefixer, postcss, etc.).
    Refactors page layout for consistent styling and reduces simulated wait duration.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { Suspense } from 'react';
 
 export default async function HomePage() {
   return (
-    <>
+    <div className='bg-slate-100 text-slate-900 min-h-screen px-5'>
       <header>
         <h1 className='text-6xl font-bold text-center py-8'>Header</h1>
       </header>
@@ -15,6 +15,6 @@ export default async function HomePage() {
       <footer>
         <p className='text-5xl font-bold text-center py-8'>Footer</p>
       </footer>
-    </>
+    </div>
   );
 }
