a31eb215843186b499d2c475a93d5427700f8bfe
MarvinRF <marvinvca@outlook.com>
2025-06-22 23:46:29 -0300

    Enhances homepage layout and content
    Updates image styling and content structure for improved UI consistency. Introduces a timestamp, a prominent title link, and descriptive text to enhance presentation and readability.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,21 +21,35 @@ export default async function HomePage() {
       >
         <Link href='#' className='w-full h-full overflow-hidden rounded-xl'>
           <Image
-            className='group-hover:scale-105 transition duration-500 ease-in-out'
+            className='w-full h-full object-cover object-center group-hover:scale-105 transition duration-500 ease-in-out'
             src='/images/bryen_0.png'
             width={1200}
             height={720}
             alt='Title'
+            priority
           />
         </Link>
-        <div>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
-          aspernatur quisquam eligendi necessitatibus est corrupti dolores quo
-          assumenda, iste modi et sequi quas, facilis perspiciatis. Cumque natus
-          at recusandae eligendi! Lorem, ipsum dolor sit amet consectetur
-          adipisicing elit. Iste alias animi optio placeat voluptatem provident
-          earum ratione amet facere eaque, omnis ut atque, odio, nihil porro
-          ipsum quis tempora! Voluptates.
+        <div className='flex flex-col sm:justify-center'>
+          <time className='text-zinc-600 text-sm/tight' dateTime='2025-04-20'>
+            20/04/2025 10:00
+          </time>
+          <h1
+            className={clsx(
+              'text-2xl/tight font-extrabold mb-4',
+              'sm:text-4xl/tight',
+            )}
+          >
+            <Link href='#'>Very big Title of a Post</Link>
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+            aspernatur quisquam eligendi necessitatibus est corrupti dolores quo
+            assumenda, iste modi et sequi quas, facilis perspiciatis. Cumque
+            natus at recusandae eligendi! Lorem, ipsum dolor sit amet
+            consectetur adipisicing elit. Iste alias animi optio placeat
+            voluptatem provident earum ratione amet facere eaque, omnis ut
+            atque, odio, nihil porro ipsum quis tempora! Voluptates.
+          </p>
         </div>
       </section>
       <Suspense fallback={<SpinLoader />}>
