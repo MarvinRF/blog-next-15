2815e1e1ea73ce4d7ad597b2e7cbbaae174c27f9
Marvin Rocha <marvinvca@outlook.com>
2025-07-18 23:45:59 -0300

    fix types on slug page

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -2,33 +2,30 @@ import SinglePost from '@/components/SinglePost';
 import { SpinLoader } from '@/components/SpinLoader';
 import { findPostBySlugCached } from '@/lib/post/queries';
 import { Metadata } from 'next';
-import React, { Suspense } from 'react';
+import { Suspense } from 'react';
 
 type PostSlugPageProps = {
   params: Promise<{ slug: string }>;
 };
 
-export const generateMetaData = async ({
+export async function generateMetadata({
   params,
-}: PostSlugPageProps): Promise<Metadata> => {
+}: PostSlugPageProps): Promise<Metadata> {
   const { slug } = await params;
   const post = await findPostBySlugCached(slug);
+
   return {
     title: post.title,
     description: post.excerpt,
   };
-};
+}
 
-const PostSlugPage = async ({ params }: PostSlugPageProps) => {
+export default async function PostSlugPage({ params }: PostSlugPageProps) {
   const { slug } = await params;
 
   return (
-    <>
-      <Suspense fallback={<SpinLoader />}>
-        <SinglePost slug={slug} />
-      </Suspense>
-    </>
+    <Suspense fallback={<SpinLoader className='min-h-20 mb-16' />}>
+      <SinglePost slug={slug} />
+    </Suspense>
   );
-};
-
-export default PostSlugPage;
+}
